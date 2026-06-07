0a52242d1715b6c375338c4a41a76a80d6b534bd
puneetchaurasia10-ai <puneetchaurasia10@gmail.com>
2026-06-07 17:38:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,65 @@
 
 function hideAllPages(){
-document.querySelectorAll('.page').forEach(page=>{
-page.classList.remove('active');
-});
+  document.querySelectorAll('.page').forEach(page=>{
+    page.classList.remove('active');
+  });
 }
 
 function checkPwd(){
 
-const pwd=document.getElementById('pwd').value;
+  const pwd=document.getElementById('pwd').value;
 
-if(pwd==="200704"){
+  if(pwd==="200704"){
 
-hideAllPages();
+    hideAllPages();
+    document.getElementById('homePage').classList.add('active');
 
-document.getElementById('homePage').classList.add('active');
+  }else{
 
-}else{
+    alert("Wrong Password ❌");
 
-alert("Wrong Password ❌");
-
-}
+  }
 
 }
 
 function showGiftPage(){
 
-hideAllPages();
-
-document.getElementById('giftPage').classList.add('active');
+  hideAllPages();
+  document.getElementById('giftPage').classList.add('active');
 
 }
 
 function openGift1(){
 
-hideAllPages();
-
-document.getElementById('gift1Page').classList.add('active');
+  hideAllPages();
+  document.getElementById('gift1Page').classList.add('active');
 
 }
 
 function openGift2(){
 
-hideAllPages();
-
-document.getElementById('gift2Page').classList.add('active');
+  hideAllPages();
+  document.getElementById('gift2Page').classList.add('active');
 
 }
 
 function openGift3(){
 
-hideAllPages();
-
-document.getElementById('gift3Page').classList.add('active');
+  hideAllPages();
+  document.getElementById('gift3Page').classList.add('active');
 
 }
 
 function backHome(){
 
-hideAllPages();
-
-document.getElementById('homePage').classList.add('active');
+  hideAllPages();
+  document.getElementById('homePage').classList.add('active');
 
 }
 
 function backGift(){
 
-hideAllPages();
-
-document.getElementById('giftPage').classList.add('active');
+  hideAllPages();
+  document.getElementById('giftPage').classList.add('active');
 
 }
